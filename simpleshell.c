29a65ec633a66e7ac16985efb94f9991001a41fd
boydascott <boydascott@outlook.com>
2026-01-27 12:41:21 +0000

    finished test exit logic

--- simpleshell.c
+++ simpleshell.c
@@ -3,18 +3,30 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <string.h>
 
 int main (void) {
-  char buffer[512];
+  char* buffer = malloc(sizeof(char) * 512);
 
   printf("> ");
   fgets(buffer, sizeof(buffer), stdin);
+  buffer[strcspn(buffer, "\n")] = 0; // deletes new line
 
-  while (*buffer != 'q') {
-    printf("%s", buffer);
+  printf("%s", buffer);
+
+  if (buffer == NULL) {
+      printf("ctrl-d");
+  }
+  
+  while (strcmp(buffer, "exit") != 0 && fgets(buffer, sizeof(buffer), stdin) != NULL) {
+    printf("%s\n", buffer);
     printf("> ");
     fgets(buffer, sizeof(buffer), stdin);
+    buffer[strcspn(buffer, "\n")] = 0;
   }
+  
+  free(buffer);
+  buffer = NULL;
 
   return 0;
 }

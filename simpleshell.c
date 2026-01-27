982e71e689f8fcb9d87344d0de3abedfb7c8b510
Ryan Laird <ryanlaird47@yahoo.com>
2026-01-27 12:31:14 +0000

    Update simpleshell.c
    added split string

--- simpleshell.c
+++ simpleshell.c
@@ -3,15 +3,29 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <string.h>
+
+void splitstring(char buffer[],char delims[]) {  
+  char* token = strtok(buffer,delims);
+
+     while(token != NULL){
+          printf("%s\n", token);
+
+          token = strtok(NULL, delims);
+     }
+  
+  return ;
+}
 
 int main (void) {
   char buffer[512];
-
+  char delims[] = " \t|><&;";
   printf("> ");
   fgets(buffer, sizeof(buffer), stdin);
-
+  
+  
   while (*buffer != 'q') {
-    printf("%s", buffer);
+    splitstring(buffer, delims);
     printf("> ");
     fgets(buffer, sizeof(buffer), stdin);
   }
